386489847d43501d52d8f57cf269da2ac73b0851
99andrew99 <whalstjr199927@gmail.com>
2026-01-15 17:52:01 +0900

    [Chore] pretendard 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./Providers";
+import localFont from "next/font/local";
+
+const pretendard = localFont({
+    src: "../assets/fonts/PretendardVariable.woff2",
+    display: "swap",
+    weight: "45 920",
+    variable: "--font-pretendard",
+});
 
 export const metadata: Metadata = {
     title: "같이가게",
@@ -16,7 +24,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="ko">
+        <html lang="ko" className={`${pretendard.variable}`}>
             <body className="antialiased">
                 <Providers>{children}</Providers>
             </body>
---
+++ src/components/atoms/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="w-full h-[80px]">
+            <div></div>
+        </div>
+    );
+}
