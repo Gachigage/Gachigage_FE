acac21b4fcbb7eae12cdabfc341e682d38576241
99andrew99 <whalstjr199927@gmail.com>
2026-01-16 13:43:27 +0900

    [Design] default black color #160502로 변경

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -1,7 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <div className="w-full h-[80px]">
-            <div></div>
+        <div className="w-full h-[80px] flex items-center justify-center bg-amber-200">
+            <div className="w-full max-w-[402px] xl:max-w-[1152px] bg-amber-500 flex">
+                <div className="flex gap-[20px]">
+                    <Image
+                        src="/logo.svg"
+                        alt="같이가게 로고"
+                        width={40}
+                        height={40}
+                        className="cursor-pointer"
+                    />
+
+                    <div className="flex gap-[20px]">
+                        <Link
+                            href="/products"
+                            className="text-[18px] py-[6.5px] px-[10px] cursor-pointer"
+                        >
+                            물품 거래
+                        </Link>
+                        <Link
+                            href="/community"
+                            className="text-[18px] py-[6.5px] px-[10px] cursor-pointer"
+                        >
+                            커뮤니티
+                        </Link>
+                        <Link
+                            href="/chat"
+                            className="text-[18px] py-[6.5px] px-[10px] cursor-pointer"
+                        >
+                            채팅
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
