16dd4ee6404dc73243f9f30edc6362e1180507fb
99andrew99 <whalstjr199927@gmail.com>
2026-01-16 14:25:37 +0900

    Revert "Merge pull request #6 from Gachigage/feature/header"
    This reverts commit 10a637c62133334e3f692d8043e3a8d6a7f31a8b, reversing
    changes made to abc0b969de264f25e3e3f8be69928b8892629067.

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,14 +1,7 @@
-import Header from "@/components/atoms/Header";
-
 export default function ProductLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return (
-        <div className="flex flex-col w-screen">
-            <Header />
-            {children}
-        </div>
-    );
+    return <div>{children}</div>;
 }
