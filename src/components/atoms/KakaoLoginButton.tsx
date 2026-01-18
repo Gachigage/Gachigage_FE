1d5191477f02ddd2c29841bafe726eeb00678938
99andrew99 <whalstjr199927@gmail.com>
2026-01-18 15:48:56 +0900

    [Feat] 로그인 페이지 구현

--- src/components/atoms/KakaoLoginButton.tsx
+++ src/components/atoms/KakaoLoginButton.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import kakao from "@/assets/icons/kakao.svg";
--- src/components/atoms/LogoutButton.tsx
+++ src/components/atoms/LogoutButton.tsx
@@ -2,6 +2,7 @@
 
 import { signOut } from "next-auth/react";
 
+// 임시 로그아웃 버튼 추후 수정
 export function LogoutButton() {
     return (
         <button onClick={() => signOut({ callbackUrl: "/" })}>로그아웃</button>
