21bbdbd37fb1c214fc65f371983243140657bef1
99andrew99 <whalstjr199927@gmail.com>
2026-01-21 18:00:33 +0900

    [Refactor] ProductAddButton Link 태그로 변경

--- src/components/atoms/ProductAddButton.tsx
+++ src/components/atoms/ProductAddButton.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function ProductAddButton() {
     return (
-        <button className="w-[114px] h-[32px] cursor-pointer md:w-[230px] md:h-[54px] xl:w-[186px] xl:h-[56px] bg-gachigageMint border-[0.5px] border-gachigageBrightMint1 rounded-[8px] text-gachigageWhite font-normal text-[13px] md:font-medium  xl:font-semibold md:text-[18px] hover:bg-gachigageDarkMint1">
+        <Link
+            href="/products/create"
+            className="w-[114px] h-[32px] cursor-pointer md:w-[230px] md:h-[54px] xl:w-[186px] xl:h-[56px] flex items-center justify-center bg-gachigageMint border-[0.5px] border-gachigageBrightMint1 rounded-[8px] text-gachigageWhite font-normal text-[13px] md:font-medium xl:font-semibold md:text-[18px] hover:bg-gachigageDarkMint1"
+        >
             물품 등록
-        </button>
+        </Link>
     );
 }
