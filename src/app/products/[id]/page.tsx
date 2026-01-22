1ca6312922df7032db5641011c5b3fd2da107c6f
99andrew99 <whalstjr199927@gmail.com>
2026-01-22 17:20:21 +0900

    [Feat] ProductDetailImgs 컴포넌트 구현

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -16,7 +16,7 @@ export default function ProductDetailPage() {
 
     return (
         <div className="w-full flex-1 bg-gachigageWhite flex justify-center">
-            <div className="w-full pt-[140px] pb-[134px] md:pb-[60px] max-w-[402px] md:max-w-[768px] xl:max-w-[1152px] flex flex-col">
+            <div className="w-full pt-[140px] pb-[134px] md:pb-[60px] max-w-[402px] md:max-w-[1152px] md:px-[24px] flex flex-col">
                 <BackButton pageName={"판매 상품 목록"} href={"/products"} />
                 <div className="flex flex-col md:flex-row"></div>
                 <ProductDetailImgs images={imgs} />
