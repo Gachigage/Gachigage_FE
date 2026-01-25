269d19da181434401e47f5df22a4108121be0e58
99andrew99 <whalstjr199927@gmail.com>
2026-01-25 14:31:21 +0900

    [Refactor] QuantityRadio 컴포넌트 라디오 기능 제거

--- src/components/atoms/QuantityRadio.tsx
+++ src/components/atoms/QuantityRadio.tsx
@@ -1,5 +1,4 @@
 import { formatNumber } from "@/lib/utils";
-import { useState } from "react";
 
 type QuantityRadioOption = {
     quantity: number;
@@ -11,37 +10,16 @@ export default function QuantityRadio({
 }: {
     options: QuantityRadioOption[];
 }) {
-    const [selected, setSelected] = useState(0);
-
     return (
         <div className="flex flex-col gap-[8px]">
             {options.map((option, index) => (
-                <label
+                <div
                     key={index}
-                    onClick={() => setSelected(index)}
-                    className={`flex items-center font-medium justify-between px-[12px] py-[7px] cursor-pointer border-[1px] rounded-[8px] ${
-                        selected === index
-                            ? "border-gachigageMint bg-gachigageBrightMint3 text-gachigageDark"
-                            : "border-gachigageGray1 bg-white text-gachigageGray7 "
-                    }`}
+                    className="flex items-center font-medium justify-between px-[12px] py-[7px] border-[1px] rounded-[8px] border-gachigageGray1 bg-white text-gachigageGray7"
                 >
-                    <div className="flex items-center gap-[8px]">
-                        <div
-                            className={`w-[14px] h-[14px] rounded-full border-[1px] flex items-center justify-center ${
-                                selected === index
-                                    ? "border-gachigageMint"
-                                    : "border-gachigageGray7"
-                            }`}
-                        >
-                            {selected === index && (
-                                <div className="w-[6px] h-[6px] rounded-full bg-gachigageMint" />
-                            )}
-                        </div>
-                        <span>{option.quantity}개</span>
-                    </div>
-
+                    <span>{option.quantity}개</span>
                     <span>{formatNumber(option.price)}원</span>
-                </label>
+                </div>
             ))}
         </div>
     );
