e2d486e5a4972db3038efdb1ff65021f6aa02ebe
99andrew99 <whalstjr199927@gmail.com>
2026-02-04 19:35:04 +0900

    [Refactor] Search 컴포넌트 검색 버튼 누를 때 동작하도록 수정

--- src/components/atoms/Search.tsx
+++ src/components/atoms/Search.tsx
@@ -3,18 +3,17 @@
 import Image from "next/image";
 import searchIcon from "@/assets/icons/search.svg";
 import { useProductSearchFilterStore } from "@/store/useProductSearchFilterStore";
+import { useState } from "react";
 
 export default function Search(props: { placeholderText?: string }) {
-    const searchKeyword = useProductSearchFilterStore(
-        (state) => state.searchKeyword,
-    );
+    const [search, setSearch] = useState("");
+
     const setSearchKeyword = useProductSearchFilterStore(
         (state) => state.setSearchKeyword,
     );
 
     const handleSearch = () => {
-        if (!searchKeyword.trim()) return;
-        console.log(searchKeyword);
+        setSearchKeyword(search);
     };
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -28,8 +27,8 @@ export default function Search(props: { placeholderText?: string }) {
             <Image src={searchIcon} alt="검색 아이콘" width={24} height={24} />
             <input
                 type="text"
-                value={searchKeyword}
-                onChange={(e) => setSearchKeyword(e.target.value)}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
                 onKeyDown={handleKeyDown}
                 placeholder={props.placeholderText || "검색어를 입력하세요"}
                 className="flex-1 placeholder:text-gachigageGray5 font-normal focus:outline-none mr-[14px]"
