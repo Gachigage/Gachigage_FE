31175b40aaed60abd16d1127c2cbb6e993bdd234
99andrew99 <whalstjr199927@gmail.com>
2026-02-06 21:41:31 +0900

    [Feat] 상품 없을 때 표시할 텍스트 구현

--- src/components/organisms/ProductLanding.tsx
+++ src/components/organisms/ProductLanding.tsx
@@ -7,6 +7,8 @@ import ProductGroupFilter from "../atoms/ProductGroupFilter";
 import ProductList from "../molecules/ProductList";
 import { useProductList } from "@/hooks/useProductList";
 import { useEffect } from "react";
+import Image from "next/image";
+import searchIcon from "@/assets/icons/search.svg";
 
 export default function ProductLanding() {
     const { productPage, setProductPage } = useProductSearchFilterStore();
@@ -28,12 +30,23 @@ export default function ProductLanding() {
                 <ProductAddButton />
             </div>
             <ProductList products={products} />
-            {totalPages > 0 && (
+            {totalPages > 0 ? (
                 <Pagenation
                     currentPage={productPage.currentPage}
                     totalPages={totalPages}
                     onPageChange={handlePageChange}
                 />
+            ) : (
+                <div className="w-full flex items-center justify-center text-[16px] md:text-[18px] xl:text-[24px] text-gachigageGray5 font-normal pt-[145px] xl:pt-[184px] gap-[12px]">
+                    <Image
+                        src={searchIcon}
+                        alt="searchIcon"
+                        width={12}
+                        height={12}
+                        className="md:w-[17px] md:h-[17px] xl:w-[26px] xl:h-[26px]"
+                    />
+                    <span>검색하신 유형의 물품을 찾을 수 없습니다</span>
+                </div>
             )}
         </div>
     );
