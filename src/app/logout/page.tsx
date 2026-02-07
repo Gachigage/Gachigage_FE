b7f03839a9fd6fb7b807da11230e2c5e04b3ae93
99andrew99 <whalstjr199927@gmail.com>
2026-02-07 12:52:15 +0900

    [Feat] 임시 로그아웃 페이지 구현

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,9 @@
+import { LogoutButton } from "@/components/atoms/LogoutButton";
+
+export default function LogoutPage() {
+    return (
+        <div className="w-screen h-screen flex items-center justify-center bg-gachigageWhite">
+            <LogoutButton />
+        </div>
+    );
+}
